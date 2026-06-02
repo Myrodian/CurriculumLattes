89169213c10a63dad5f1ab4aebccc7d83972874e
NasserMeneses <menesesnasser@gmail.com>
2026-06-01 22:13:34 -0300

    feat: adcionando paginas : Apresentacao, produto, projetoEnsino, trabalhos Tecnicos

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,26 +3,33 @@ import LoginPage from './pages/LoginPage';
 import CadastroPage from './pages/CadastroPage';
 import ProfilePage from './pages/ProfilePage';
 import FeedPage from './pages/FeedPage';
+import ApresentacaoTrabalhoPage from './pages/ApresentacaoPage';
+import ProdutoPage from './pages/ProdutoPage';
+import ProjetoEnsinoPage from './pages/ProjetoEnsinoPage';
+import TrabalhosTecnicosPage from './pages/TrabalhosTecnicosPage';
 import PrivateRoute from './components/PrivateRoute';
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/cadastro" element={<CadastroPage />} />
-      <Route path="/perfil" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
-      <Route path="/feed" element={<PrivateRoute><FeedPage /></PrivateRoute>} />
-      <Route
-        path="/dashboard"
-        element={
-          <PrivateRoute>
-            <h1>Dashboard (protegido)</h1>
-          </PrivateRoute>
-        }
-      />
-      <Route path="*" element={<Navigate to="/login" />} />
-    </Routes>
-  );
+    return (
+        <Routes>
+            {/* Rotas públicas */}
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/cadastro" element={<CadastroPage />} />
+
+            {/* Rotas protegidas — existentes */}
+            <Route path="/perfil" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
+            <Route path="/feed" element={<PrivateRoute><FeedPage /></PrivateRoute>} />
+            <Route path="/dashboard" element={<PrivateRoute><h1>Dashboard (protegido)</h1></PrivateRoute>} />
+
+            {/* Rotas protegidas — produção acadêmica */}
+            <Route path="/apresentacao" element={<PrivateRoute><ApresentacaoTrabalhoPage /></PrivateRoute>} />
+            <Route path="/produto" element={<PrivateRoute><ProdutoPage /></PrivateRoute>} />
+            <Route path="/projeto-ensino" element={<PrivateRoute><ProjetoEnsinoPage /></PrivateRoute>} />
+            <Route path="/trabalhos-tecnicos" element={<PrivateRoute><TrabalhosTecnicosPage /></PrivateRoute>} />
+
+            <Route path="*" element={<Navigate to="/login" />} />
+        </Routes>
+    );
 }
 
 export default App;
